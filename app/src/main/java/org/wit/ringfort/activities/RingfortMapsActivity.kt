ecd672104ba1e420248a20a3ec7fd4ea99136ac7
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2019-12-30 13:11:09 +0000

    update map cardview

--- app/src/main/java/org/wit/ringfort/activities/RingfortMapsActivity.kt
+++ app/src/main/java/org/wit/ringfort/activities/RingfortMapsActivity.kt
@@ -2,16 +2,68 @@ package org.wit.ringfort.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import kotlinx.android.synthetic.main.activity_ringfort_maps.*
+import kotlinx.android.synthetic.main.content_ringfort_maps.*
+
 import org.wit.ringfort.R
+import org.wit.ringfort.main.MainApp
 
 class RingfortMapsActivity : AppCompatActivity() {
+
+    lateinit var map: GoogleMap
+    lateinit var app: MainApp
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ringfort_maps)
-        toolbar.title = title
+//        toolbar.title = title
         setSupportActionBar(toolbar)
+        app = application as MainApp
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
+    }
+
+    fun configureMap() {
+        map.uiSettings.setZoomControlsEnabled(true)
+        app.ringforts.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
